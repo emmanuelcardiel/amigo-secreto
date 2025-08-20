a62f74e29c8971bf2e0009bcb70d4e7fec1afce4
emmanuelcardiel <licfisca83@gmail.com>
2025-08-19 22:39:35 -0600

    instrucciones

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 consol.log("ejecutando el sistema")
 
 function verificarIngreso() {
-    let amigos : []
+    let amigos : [];
+        amigos [] : "";
+    let escribeUnNombre = document.getElementById("amigo").value
+
+    //Capturar el valor del campo de entrada: Utilizar document.getElementById o document.querySelector para obtener el texto ingresado por el usuario.
+
+//Validar la entrada: Implementar una validación para asegurarse de que el campo no esté vacío. Si está vacío, mostrar un alert con un mensaje de error: "Por favor, inserte un nombre."
+
+//Actualizar el array de amigos: Si el valor es válido, añadirlo al arreglo que almacena los nombre de amigos usando el método.push().
+
+//Limpiar el campo de entrada: Después de añadir el nombre, restablecer el campo de texto a una cadena vacía.
 }
